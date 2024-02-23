de06d6970ce53da8aa3b6ce041af311ec3c34511
Nalia Basova <umz-strahova@mail.ru>
2024-02-23 18:37:26 +0300

    2 задание

--- homework_01/main.py
+++ homework_01/main.py
@@ -11,7 +11,8 @@ def power_numbers(*num):
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-    return list(map(lambda x: x ** 2,  num))
+    return list(map(lambda x: x**2, num))
+
 
 # filter types
 ODD = "odd"
@@ -19,7 +20,22 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def is_simple(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def is_odd(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+
+def filter_numbers(numbers, selector):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +46,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if selector == PRIME:
+        return list(filter(lambda num: is_simple(num), list(numbers)))
+    elif selector == ODD:
+        return list(filter(lambda num: not is_odd(num), list(numbers)))
+    elif selector == EVEN:
+        return list(filter(lambda num: is_odd(num), list(numbers)))
