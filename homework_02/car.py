e2fa1cf6835f48ef58e0fff31c6b620092553940
Nalia Basova <umz-strahova@mail.ru>
2024-03-14 14:47:57 +0300

    все еще домшка

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,18 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+
+from homework_02.base import Vehicle
+
+
+class Car(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, engine):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = engine
+
+    def set_engine(engine):
+        pass
+
+
+# car1 = Car()
+# car1.set_engine()
