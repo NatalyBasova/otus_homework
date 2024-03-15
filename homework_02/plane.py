beb8372a44dabbcbaac3376d3d75f9d7317051e3
Nalia Basova <umz-strahova@mail.ru>
2024-03-15 18:37:23 +0300

    plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -3,7 +3,17 @@
 """
 
 from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
 
 
 class Plane(Vehicle):
-    pass
+    def __init__(self, weight, fuel, fuel_consumption, engine, cargo, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption, engine)
+        self.cargo = cargo
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, weight):
+        if weight + self.cargo <= self.max_cargo:
+            self.cargo += weight
+        else:
+            raise CargoOverload
