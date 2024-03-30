2c162a534272239ba7cf6a31533f737119e49583
Nalia Basova <umz-strahova@mail.ru>
2024-03-30 19:10:54 +0300

    server works

--- homework_03/webapp/main.py
+++ homework_03/webapp/main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+from typing import Union
+
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.head("/")
+@app.get("/")
+def read_root():
+    return {"message": "ok"}
+
+@app.get("/ping/")
+def pong():
+    return {"ping": "pong!"} 
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
