f29a3bd6d006e8018c94094de05be0f055e54745
Nalia Basova <umz-strahova@mail.ru>
2024-04-21 17:08:20 +0300

    create main func

--- homework_03/webapp/main.py
+++ homework_03/webapp/main.py
@@ -7,15 +7,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.head("/")
 @app.get("/")
 def read_root():
     return {"message": "ok"}
 
+
 @app.get("/ping/")
 def pong():
-    return {"ping": "pong!"} 
+    return {"ping": "pong!"}
+
+
+def main():
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    main()
