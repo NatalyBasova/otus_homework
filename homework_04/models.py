a824ca5edc27fcc3298df36b1ab3c4c6f3957bc1
Nalia Basova <umz-strahova@mail.ru>
2024-06-16 22:00:24 +0300

    try to build model

--- homework_04/models.py
+++ homework_04/models.py
@@ -10,7 +10,84 @@
 
 import os
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+)
+import config
+
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    declared_attr,
+)
+
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    ForeignKey,
+    String,
+)
+from sqlalchemy.orm import relationship
+
+
+PG_CONN_URI = (
+    os.environ.get("SQLALCHEMY_PG_CONN_URI")
+    or "postgresql+asyncpg://postgres:password@localhost/postgres"
+)
 
 Base = None
 Session = None
+
+async_engine = create_async_engine(
+    url=config.DB_URL,
+    echo=config.DB_ECHO,
+)
+async_session = async_sessionmaker(
+    bind=async_engine,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
+
+    id = Column(Integer, primary_key=True)
+
+
+class User(Base):
+
+    name = Column(String)
+    username = Column(String(32), nullable=False, unique=True)
+    email = Column(String, nullable=True, unique=True)
+
+
+user = relationship("User", backref="Posts")
+
+
+class Post(Base):
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        unique=False,
+        nullable=False,
+    )
+    title = Column(
+        String(80),
+        nullable=False,
+        default="",
+        server_default="",
+    )
+    body = Column(Text, default="", server_default="")
+
+    post = relationship("Post", backref="User")
+
+    def __repr__(self):
+        return str(self)
