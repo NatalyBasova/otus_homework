b80a98c016634228006608df1b17cb7660b2003a
NatalyBasova <161022788+NatalyBasova@users.noreply.github.com>
2024-06-24 21:33:50 +0300

    homework_04 (#4)
    * set reqs
    
    * set fake data urls
    
    * get resources
    
    * get users list
    
    * create users in one commit
    
    * create posts
    
    * create relationships
    
    * create tables before execution
    
    * close session

---
+++ homework_04/main.py
@@ -0,0 +1,76 @@
+"""
+Домашнее задание №4
+Асинхронная работа с сетью и бд
+
+доработайте функцию main, по вызову которой будет выполняться полный цикл программы
+(добавьте туда выполнение асинхронной функции async_main):
+- создание таблиц (инициализация)
+- загрузка пользователей и постов
+    - загрузка пользователей и постов должна выполняться конкурентно (параллельно)
+      при помощи asyncio.gather (https://docs.python.org/3/library/asyncio-task.html#running-tasks-concurrently)
+- добавление пользователей и постов в базу данных
+  (используйте полученные из запроса данные, передайте их в функцию для добавления в БД)
+- закрытие соединения с БД
+"""
+
+import asyncio
+
+
+import jsonplaceholder_requests
+import models
+
+
+async def async_main():
+    
+    async with models.async_engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
+
+    async with models.Session() as session:
+
+        
+
+        users_json = await jsonplaceholder_requests.fetch_json(
+            jsonplaceholder_requests.USERS_DATA_URL
+        )
+
+        posts_json = await jsonplaceholder_requests.fetch_json(
+            jsonplaceholder_requests.POSTS_DATA_URL
+        )
+
+        users_list = list()
+        for json_obj in users_json:
+            filtered_fields = dict(
+                {
+                    "name": json_obj.pop("name"),
+                    "username": json_obj.pop("username"),
+                    "email": json_obj.pop("email"),
+                    "id": json_obj.pop("id"),
+                }
+            )
+
+            users_list.append(models.User(**filtered_fields))
+
+        posts_list = list()
+        for json_obj in posts_json:
+            filtered_fields = dict(
+                {
+                    "user_id": json_obj.pop("userId"),
+                    "title": json_obj.pop("title"),
+                    "body": json_obj.pop("body"),
+                }
+            )
+
+            posts_list.append(models.Post(**filtered_fields))
+
+        await models.create_users(session=session, users=users_list)
+        await models.create_posts(session=session, posts=posts_list)
+        
+        session.close_all()
+
+def main():
+
+    asyncio.run(async_main())
+
+
+if __name__ == "__main__":
+    main()
---
+++ homework_04/models.py
@@ -0,0 +1,121 @@
+"""
+создайте алхимичный engine
+добавьте declarative base (свяжите с engine)
+создайте объект Session
+добавьте модели User и Post, объявите поля:
+для модели User обязательными являются name, username, email
+для модели Post обязательными являются user_id, title, body
+создайте связи relationship между моделями: User.posts и Post.user
+"""
+
+import os
+from typing import Sequence, List
+
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    declared_attr,
+    relationship,
+)
+
+
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    ForeignKey,
+    String,
+    Text,
+)
+
+
+PG_CONN_URI = (
+    os.environ.get("SQLALCHEMY_PG_CONN_URI")
+    or "postgresql+asyncpg://postgres:password@localhost/postgres"
+)
+
+
+class Base(DeclarativeBase):
+    @declared_attr
+    def __tablename__(cls):
+        return f"{cls.__name__.lower()}s"
+
+    id = Column(Integer, primary_key=True)
+
+    def __repr__(self) -> str:
+        fields = self.__dict__.copy()
+        fields.pop("_sa_instance_state")
+        return str(fields)
+
+    def __init__(self, iterable=(), **kwargs):
+        self.__dict__.update(iterable, **kwargs)
+
+
+class User(Base):
+
+    name = Column(String)
+    username = Column(String(32), nullable=False, unique=True)
+    email = Column(String, nullable=True, unique=True)
+
+    posts = relationship(
+        "Post",
+        back_populates="user",
+        uselist=True,
+    )
+
+
+class Post(Base):
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+        unique=False,
+        nullable=False,
+    )
+    title = Column(
+        String(80),
+        nullable=False,
+        default="",
+        server_default="",
+    )
+    body = Column(Text, default="", server_default="")
+
+    user = relationship("User", back_populates="posts", uselist=False)
+
+
+async_engine = create_async_engine(url=PG_CONN_URI, echo=True)
+Session = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    autocommit=False,
+    expire_on_commit=False,
+)
+
+
+async def recreate_models():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def create_users(
+    session: AsyncSession,
+    users: List[User],
+):
+    session.add_all(users)
+    await session.commit()
+
+
+async def create_posts(
+    session: AsyncSession,
+    posts: List[Post],
+):
+    session.add_all(posts)
+    await session.commit()
