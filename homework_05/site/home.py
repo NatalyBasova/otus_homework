95422b4752ff8f558d26a76982df4bc383dac61b
Nalia Basova <umz-strahova@mail.ru>
2024-06-29 23:17:18 +0300

    fix navbar

--- homework_05/site/home.py
+++ homework_05/site/home.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, render_template, request, send_from_directory
+from .utils import *
 
 bp = Blueprint("home", __name__)
 
 
 @bp.route("/")
 def index():
-
-    return render_template("home/index.html")
+    mc = set_menu("home")
+    return render_template("home/index.html",mc=mc)
 
 
 @bp.route("/about/")
 def about():
 
-    return render_template("home/about.html")
+    mc = set_menu("about")
+    return render_template("home/about.html",mc=mc)
